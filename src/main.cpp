4ee22b7e6484c71135e6d363cb4bfa4130bbcc02
Yaakov Haimoff <hyaakov@aol.com>
2023-08-16 15:00:38 +0300

    added CaptureCamera, taking a picture with the rasperrypi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#include "CaptureCamera.h"
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main ()
 {
-	std::cout << "Hello World!" << std::endl;
-	return EXIT_SUCCESS;
+    //Input your desired filepath in place of the filepath denoted here
+    char filename[] = "/home/yakovhaimoff/Documents/image1.jpg";
+    CaptureCamera camera;
+    camera.takePic(filename);
+    cout<<"Image successful.."<<endl;
+
+    return 0;
 }
