1db24a1499062cd8d9c6aaa9e9947bdd65be1af6
Yaakov Haimoff <hyaakov@aol.com>
2023-08-16 13:33:30 +0300

    added controller class

---
+++ include/Controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yaakov Haimoff on 16/08/2023.
+//
+
+#include "Controller.h"
---
+++ include/Controller.h
@@ -0,0 +1,14 @@
+//
+// Created by Yaakov Haimoff on 16/08/2023.
+//
+
+#ifndef FARM_GUARD_CONTROLLER_H
+#define FARM_GUARD_CONTROLLER_H
+
+
+class Controller {
+
+};
+
+
+#endif //FARM_GUARD_CONTROLLER_H
