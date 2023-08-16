2ea3b9c1bdb3e2669b70c7dafdde635a9916a0a2
Yaakov Haimoff <hyaakov@aol.com>
2023-08-16 16:09:48 +0300

    camera capture changed to be used with opencv

--- include/CaptureCamera.h
+++ include/CaptureCamera.h
@@ -1,11 +1,19 @@
 # pragma once
 
 #include <csignal>
+#include <iostream>
 #include <unistd.h>
 #include <string>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
 
 class CaptureCamera {
+
+    VideoCapture m_cap;
+
     public:
+    CaptureCamera();
     void takePic(char* filename);
 };
 
--- src/CaptureCamera.cpp
+++ src/CaptureCamera.cpp
@@ -1,17 +1,25 @@
 #include "CaptureCamera.h"
 
-static pid_t pid = 0;
-
-void takePic (char* filename)
-{
-    if((pid = fork()) == 0)
-    {
-        execl("/usr/bin/raspistill",
-              "/usr/bin/raspistill",
-              "-n",
-              "-vf",
-              "-o",
-              filename,
-              NULL);
+CaptureCamera::CaptureCamera() {
+    m_cap.open(0);
+    if (!m_cap.isOpened()) {
+        std::cout << "Camera not opened" << std::endl;
+        exit(0);
     }
+}
+
+void CaptureCamera::takePic(char *filename) {
+
+    Mat frame;
+    m_cap >> frame;
+    if (frame.empty()) {
+        std::cout << "No frame captured" << std::endl;
+        return;
+    }
+    // send the frame to detect a movement
+
+    imshow("image captured: ", frame);
+
+    m_cap.release();
+    destroyAllWindows();
 }
